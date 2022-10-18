e0d379318b85eea9adbef593e90d1598ba9bd42c
mustafanysan <mustafa.nysan@mail.ru>
2022-10-18 21:45:39 +0600

    Initial commit

---
+++ lesson19_project_hard_source-main/service/decorators.py
@@ -0,0 +1,45 @@
+import jwt
+from flask import request, current_app
+
+from implemented import user_service
+
+
+def auth_required(func):
+    def wrapper(*args, **kwargs):
+        token = request.headers.environ.get('HTTP_AUTHORIZATION', '').replace('Bearer ', '')
+        if not token:
+            return "Вы не передали токен в заголовке"
+        try:
+            jwt.decode(token,
+                       key=current_app.config['SECRET_KEY'],
+                       algorithms=current_app.config['ALGORITHM'])
+            return func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+            return "Ошибка в валидации токена"
+
+    return wrapper
+
+
+def admin_required(func):
+    def wrapper(*args, **kwargs):
+        token = request.headers.environ.get('HTTP_AUTHORIZATION', '').replace('Bearer ', '')
+
+        if not token:
+            return "Вы не передали токен в заголовке"
+        try:
+            data = jwt.decode(token, key=current_app.config['SECRET_KEY'],
+                              algorithm=current_app.config['ALGORITHM'])
+            user = user_service.get_by_username(data.get("username"))
+
+            if user:
+                if not user.pole == "admin":
+                    return "Вам доступ запрещен"
+
+            return func(*args, **kwargs)
+
+        except Exception as e:
+            print(e)
+            return "Ошибка в валидации токена"
+
+    return wrapper
